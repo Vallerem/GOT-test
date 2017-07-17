d619cf9c78779e49579a765f662a8e8f46425f7d
Manuel Valle <vallerem17@gmail.com>
2017-07-17 20:36:06 +0200

    Valar Dohaeris

--- js/main.js
+++ js/main.js
@@ -1,19 +1,18 @@
 
-
-charArr = ["dany","tyr","jon"];
+charArr = ["dany", "tyr", "jon"];
 
 function toggleVisibility(id) {
-       var e = document.getElementById(id);
+    var e = document.getElementById(id);
 
-       if(e.style.display === 'block'){
-          e.style.display = 'none';
-       } else{
-          charArr.forEach(function(elem) {
-              console.log(elem);
-              if (document.getElementById(elem).style.display !== "none" ) {
-                  document.getElementById(elem).style.display = 'none';
-              }     
-             e.style.display = 'block';
-          }, this);
-       }
+    if (e.style.display === 'block') {
+        e.style.display = 'none';
+    } else {
+        charArr.forEach(function (elem) {
+            console.log(elem);
+            if (document.getElementById(elem).style.display !== "none") {
+                document.getElementById(elem).style.display = 'none';
+            }
+            e.style.display = 'block';
+        }, this);
     }
+}
