275917ba452a16deafdb0ec233669a85c5579eb9
Manuel Valle <vallerem17@gmail.com>
2017-07-17 20:09:19 +0200

    Valar Morghulis

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+
+charArr = ["dany","tyr","jon"];
+
+function toggleVisibility(id) {
+       var e = document.getElementById(id);
+
+       if(e.style.display === 'block'){
+          e.style.display = 'none';
+       } else{
+          charArr.forEach(function(elem) {
+              console.log(elem);
+              if (document.getElementById(elem).style.display !== "none" ) {
+                  document.getElementById(elem).style.display = 'none';
+              }     
+             e.style.display = 'block';
+          }, this);
+       }
+    }
